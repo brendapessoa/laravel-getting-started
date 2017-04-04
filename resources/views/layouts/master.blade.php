5470b2e43f6b09cff9eb54e189f5b085c50e8991
BrendaPessoa <brenda.nunys@gmail.com>
2017-04-04 15:26:53 +0100

    setting up routes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,10 +9,12 @@
         
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link href='http://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'>
 
+        <link rel="stylesheet" href="{{URL::to('css/styles.css')}}">
     </head>
     <body>
-        @include('partial.header')
+        @include('partials.header')
         <div class="container">
             @yield('content')      
         </div>
